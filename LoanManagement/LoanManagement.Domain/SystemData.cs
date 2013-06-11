1cccbf4817ce5a7650ee308f939ddfadf7d51579
Aldrin Arciga <janaldrinarciga@gmail.com>
2013-06-11 15:42:24 +0800

    Added All Client Enity

--- LoanManagement/LoanManagement.Domain/SystemData.cs
+++ LoanManagement/LoanManagement.Domain/SystemData.cs
@@ -24,6 +24,18 @@ namespace LoanManagement.Domain
         public DbSet<Service> Services { get; set; }
         public DbSet<Deduction> Deductions { get; set; }
         public DbSet<Requirement> Requirements { get; set; }
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<HomeAddress> HomeAddresses { get; set; }
+        public DbSet<ClientContact> ClientContacts { get; set; }
+        public DbSet<Dependent> Dependents { get; set; }
+        public DbSet<Work> Works { get; set; }
+        public DbSet<Spouse> Spouses { get; set; }
+        public DbSet<Reference> References { get; set; }
+        public DbSet<TempHomeAddress> TempHomeAddresses { get; set; }
+        public DbSet<TempClientContact> TempClientContacts { get; set; }
+        public DbSet<TempDependent> TempDependents { get; set; }
+        public DbSet<TempWork> TempWorks { get; set; }
+        public DbSet<TempReference> TempReferences { get; set; }
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
@@ -57,6 +69,31 @@ namespace LoanManagement.Domain
                 .HasKey(x => x.RequirementId);
             modelBuilder.Entity<Deduction>()
                 .HasKey(x => x.DeductionId);
+            modelBuilder.Entity<Client>()
+                .HasKey(x => x.ClientID);
+            modelBuilder.Entity<HomeAddress>()
+                .HasKey(x => x.AddressID);
+            modelBuilder.Entity<ClientContact>()
+                .HasKey(x => x.ContactID);
+            modelBuilder.Entity<Dependent>()
+                .HasKey(x => x.DependentID);
+            modelBuilder.Entity<Work>()
+                .HasKey(x => x.WorkID);
+            modelBuilder.Entity<Spouse>()
+                .HasKey(x => x.SpouseID);
+            modelBuilder.Entity<Reference>()
+                .HasKey(x => x.ReferenceID);
+            modelBuilder.Entity<TempHomeAddress>()
+                .HasKey(x => x.AddressID);
+            modelBuilder.Entity<TempClientContact>()
+                .HasKey(x => x.ContactID);
+            modelBuilder.Entity<TempDependent>()
+                .HasKey(x => x.DependentID);
+            modelBuilder.Entity<TempWork>()
+                .HasKey(x => x.WorkID);
+            modelBuilder.Entity<TempReference>()
+                .HasKey(x => x.ReferenceID);
+
                 
                 
         }
@@ -205,4 +242,199 @@ namespace LoanManagement.Domain
         public int TempContactID { get; set; }
         public string Contact { get; set; }
     }
+
+    public class Client
+    {
+        public int ClientID { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string Suffix { get; set; }
+        public DateTime Birthday { get; set; }
+        public string Sex { get; set; }
+        public string Status { get; set; }
+        public string SSS { get; set; }
+        public string TIN { get; set; }
+        public string Email { get; set; }
+        public bool Active { get; set; }
+
+        public ICollection<HomeAddress> HomeAddress { get; set; }
+        public ICollection<ClientContact> ClientContact { get; set; }
+        public ICollection<Dependent> Dependent { get; set; }
+        public ICollection<Work> Work { get; set; }
+        public ICollection<Spouse> Spouse { get; set; }
+        public ICollection<Reference> Reference { get; set; }
+    }
+
+    public class HomeAddress
+    {
+        public int AddressID { get; set; }
+        public int AddressNumber { get; set; }
+        public string Street { get; set; }
+        public string Province { get; set; }
+        public string City { get; set; }
+        public string OwnershipType { get; set; }
+        public double MonthlyFee { get; set; }
+        public string LengthOfStay { get; set; }
+
+        public int ClientID { get;set; }
+        public virtual Client Client{ get; set; }
+    }
+
+    public class ClientContact
+    {
+        public int ContactID { get; set; }
+        public int ContactNumber { get; set; }
+        public string Contact { get; set; }
+        public bool Primary { get; set; }
+
+        public int ClientID { get; set; }
+        public virtual Client Client { get; set; }
+    }
+
+    public class Dependent
+    {
+        public int DependentID { get; set; }
+        public int DependentNumber { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string Suffix { get; set; }
+        public DateTime Birthday { get; set; }
+        public string School { get; set; }
+
+        public int ClientID { get; set; }
+        public virtual Client Client { get; set; }
+    }
+
+    public class Work
+    {
+        public int WorkID { get; set; }
+        public int WorkNumber { get; set; }
+        public string BusinessName { get; set; }
+        public string DTI { get; set; }
+        public string Street { get; set; }
+        public string Province { get; set; }
+        public string City { get; set; }
+        public string Employment { get; set; }
+        public string LengthOfStay { get; set; }
+        public string BusinessNumber { get; set; }
+        public string Position { get; set; }
+        public double MonthlyIncome { get; set; }
+        public string PLNumber { get; set; }
+        public string status { get; set; }
+
+        public int ClientID { get; set; }
+        public virtual Client Client { get; set; }
+    }
+
+    public class Spouse
+    {
+        public int SpouseID { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string Suffix { get; set; }
+        public DateTime Birthday { get; set; }
+        public string BusinessName { get; set; }
+        public string DTI { get; set; }
+        public string Street { get; set; }
+        public string Province { get; set; }
+        public string City { get; set; }
+        public string Employment { get; set; }
+        public string LengthOfStay { get; set; }
+        public string BusinessNumber { get; set; }
+        public string Position { get; set; }
+        public double MonthlyIncome { get; set; }
+        public string PLNumber { get; set; }
+        public string status { get; set; }
+
+        public int ClientID { get; set; }
+        public virtual Client Client { get; set; }
+    }
+
+    public class Reference
+    {
+        public int ReferenceID { get; set; }
+        public int ReferenceNumber { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string Suffix { get; set; }
+        public string Street { get; set; }
+        public string Province { get; set; }
+        public string City { get; set; }
+        public string Contact { get; set; }
+
+        public int ClientID { get; set; }
+        public virtual Client Client { get; set; }
+    }
+
+    //asd
+
+    public class TempHomeAddress
+    {
+        public int AddressID { get; set; }
+        public int AddressNumber { get; set; }
+        public string Street { get; set; }
+        public string Province { get; set; }
+        public string City { get; set; }
+        public string OwnershipType { get; set; }
+        public double MonthlyFee { get; set; }
+        public string LengthOfStay { get; set; }
+    }
+
+    public class TempClientContact
+    {
+        public int ContactID { get; set; }
+        public int ContactNumber { get; set; }
+        public string Contact { get; set; }
+        public bool Primary { get; set; }
+    }
+
+    public class TempDependent
+    {
+        public int DependentID { get; set; }
+        public int DependentNumber { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string Suffix { get; set; }
+        public DateTime Birthday { get; set; }
+        public string School { get; set; }
+    }
+
+    public class TempWork
+    {
+        public int WorkID { get; set; }
+        public int WorkNumber { get; set; }
+        public string BusinessName { get; set; }
+        public string DTI { get; set; }
+        public string Street { get; set; }
+        public string Province { get; set; }
+        public string City { get; set; }
+        public string Employment { get; set; }
+        public string LengthOfStay { get; set; }
+        public string BusinessNumber { get; set; }
+        public string Position { get; set; }
+        public double MonthlyIncome { get; set; }
+        public string PLNumber { get; set; }
+        public string status { get; set; }
+    }
+
+    public class TempReference
+    {
+        public int ReferenceID { get; set; }
+        public int ReferenceNumber { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string Suffix { get; set; }
+        public string Street { get; set; }
+        public string Province { get; set; }
+        public string City { get; set; }
+        public string Contact { get; set; }
+    }
+
+
 }
